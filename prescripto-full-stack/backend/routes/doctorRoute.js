e53c0cc263d8ef36a30b4da33f1d452e3a0066ed
Nimit Saxena <143353462+warrioroo7@users.noreply.github.com>
2025-11-27 13:05:37 +0530

    Update doctorRoute.js

--- prescripto-full-stack/backend/routes/doctorRoute.js
+++ prescripto-full-stack/backend/routes/doctorRoute.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import { loginDoctor, appointmentsDoctor, appointmentCancel, doctorList, changeAvailablity, appointmentComplete, doctorDashboard, doctorProfile, updateDoctorProfile } from '../controllers/doctorController.js';
+import { loginDoctor, appointmentsDoctor, appointmentCancel, doctorList, changeAvailablity,
+appointmentComplete, doctorDashboard, doctorProfile, updateDoctorProfile } from '../controllers/doctorController.js';
 import authDoctor from '../middleware/authDoctor.js';
 const doctorRouter = express.Router();
 
